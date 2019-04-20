a260f4105f79de5ef44c775411d6a795fb18afdb
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2019-04-20 14:27:49 -0500

    added useEffect react hook to ResourceList.js

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const ResourceList = () => {
+const ResourceList = ({ resource }) => {
    // initializes state as an empty array
    // the variable 'resources' provides access to the initial state.
    // setResources is used to change the state object
    const [resources, setResources] = useState([]);
 
-   const fetchResource = async () => {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-      this.setState({ resources: response.data });
-   }
+   // everytime ResourceList is rendered to the screen or is updated, the inner function in useEffect() runs. 
+   useEffect(() => {
+      (async resource => {
+         const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+
+         setResources(response.data);
+      })(resource); 
+   }, [
+   resource] 
+   );
+      
    
    return (
-      <div>{resources.length}</div>
+      <ul>
+         {resources.map(record => (
+            <li key={record.id}>{record.title}</li>))}
+      </ul>
    );
-   
-}
+};
 export default ResourceList;  
